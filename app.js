15757b45a44749b0c5e5bfb391b38319c0b72b6b
Andre Maciel <am.andremaciel@gmail.com>
2025-03-18 21:45:48 -0300

    Finalização

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let nomeAmigo = document.querySelector('input').value;
+    
+    if (nomeAmigo.trim() === "") { // Verifica se o campo está em branco
+        alert("Por favor, insira um nome.");
+        return;
+    }
+    
+    amigos.push(nomeAmigo);
+    limparCampo();
+    console.log(nomeAmigo);
+    listarAmigos();
+}
+
+function limparCampo() {
+    document.getElementById('amigo').value = '';
+}
+
+function listarAmigos() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+    
+    amigos.forEach(amigo => {
+        let item = document.createElement('li');
+        item.textContent = amigo;
+        lista.appendChild(item);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) { // Verifica se há amigos na lista antes de sortear
+        alert("A lista de amigos está vazia. Adicione amigos antes de sortear.");
+        return;
+    }
+    
+    let amigoAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[amigoAleatorio];
+    document.getElementById('resultado').innerHTML = amigoSorteado;
+}
